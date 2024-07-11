f8b81d1c534458f832038dbc65947b19e78c6998
Juanchi0207 <juanchi.sampieri@gmail.com>
2024-07-10 21:43:42 -0300

    Terminado V1 y V2 con justificaciones y diagramas

--- CeruttiSampieri-TPv2/src/main/java/FabricaExportador.java
+++ CeruttiSampieri-TPv2/src/main/java/FabricaExportador.java
@@ -1,14 +1,15 @@
 public class FabricaExportador {
     public static String obtenerEstrategia(String tipo, String nombreDeArchivo, Documento documento) {
+        Exportable exportador = null;
         switch (tipo) {
             case "EXCEL":
-                Exportable exportador = new Exportable(new ExportarAExcel(nombreDeArchivo));
+                exportador = new Exportable(new ExportarAExcel(nombreDeArchivo));
                 exportador.setDocumento(documento);
                 return exportador.actuar();
             case "PDF":
-                Exportable exportador1 = new Exportable(new ExportarPDF(new AdapterApachePDFBox(nombreDeArchivo)));
-                exportador1.setDocumento(documento);
-                return exportador1.actuar();
+                exportador = new Exportable(new ExportarPDF(new AdapterApachePDFBox(nombreDeArchivo)));
+                exportador.setDocumento(documento);
+                return exportador.actuar();
             default:
                 throw new IllegalArgumentException("Tipo de exportación no soportado");
         }
